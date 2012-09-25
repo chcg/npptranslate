f4aeccdafc490d3c78bbe02603f5d230b9e128e2
shaleenmishra <shaleenmishra@5fd2d300-71f8-4784-a619-fe78e5a691ca>
2012-09-25 16:32:10 +0000

    svn added
    git-svn-id: svn://svn.code.sf.net/p/npptranslate/code@11 5fd2d300-71f8-4784-a619-fe78e5a691ca

--- Common/Include/iTrOD.h
+++
@@ -1,7 +0,0 @@
-#include <map>
-
-namespace TrOD
-{
-	void Translate (std::wstring& in, std::wstring& out, const std::wstring& sourceLanguage, const std::wstring& destinationLang, const std::wstring& proxyAddress, const int& proxyPort, std::wstring& error);
-	void GetSupportedLanguages (std::map<std::wstring,std::wstring>& langList, const std::wstring& proxyAddress, const int& proxyPort, std::wstring& error) ;
-}
--- Common/Source/ERRMSG.cpp
+++
@@ -1,3 +0,0 @@
-#include "ERRMSG.h"
-
-wstring ERRMSG::TRANSLATE_ERROR_MESSAGE = L"Some error occured in translation. Please report error to plugin developer.\n\n\n";
